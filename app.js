a45421813d8c0317bcffb86b9d312a762b2f61d3
Asvc9 <amit.sheth9@gmail.com>
2021-08-12 11:00:39 -0700

    refactored code

--- app.js
+++ app.js
@@ -4,50 +4,51 @@ console.log("validate")
 
 const Horsepic = document.getElementById('horse');
 const Neigh = document.getElementById('neigh');
-const Animalclick = document.getElementById('animalclick')
-
-console.log(Horsepic);
-console.log(Neigh);
-
-Horsepic.addEventListener('click', () => {
-  Neigh.play() 
-  Animalclick.textContent = "The sound a horse makes..."
-  console.log(Animalclick.textContent);
-});
-
 
 
 const Catpic = document.getElementById('cat');
 const Meow = document.getElementById('meow');
 
-Catpic.addEventListener('click', () => {
-  Meow.play();
-  Animalclick.textContent = "The sound a cat makes..."
-});
-
 const Dogpic = document.getElementById('dog');
 const Bark = document.getElementById('bark');
 
+const Animalclick = document.getElementById('animalclick')
+
+console.log(Horsepic);
+console.log(Neigh);
+
+function playSoundAndText (sound, animal) {
+  sound.play();
+  Animalclick.textContent = `The sound an ${animal} makes...`
+
+}
+
+Horsepic.addEventListener('click', () => {
+  playSoundAndText(Neigh, "horse");
+});
+
+
+Catpic.addEventListener('click', () => {
+  playSoundAndText(Meow, 'cat');
+});
+
+
 Dogpic.addEventListener('click', () => {
-  Bark.play();
-  Animalclick.textContent = "The sound a dog makes..."
+  playSoundAndText(Bark, 'dog');
   
 });
 
 document.addEventListener('keydown', (event) => {
 
-    if (event.key === 'd') {
-      Bark.play();
-      Animalclick.textContent = "The sound a dog makes..."
-    }
-
     if (event.key === 'h') {
-      Neigh.play();
-      Animalclick.textContent = "The sound a horse makes..."
+      playSoundAndText(Neigh, 'horse');
     }
 
     if (event.key === 'c') {
-      Meow.play();
-      Animalclick.textContent = "The sound a cat makes..."
+     playSoundAndText(Meow, 'cat');
+    }
+
+    if (event.key === 'd') {
+      playSoundAndText(Bark, 'dog')
     }
 });
