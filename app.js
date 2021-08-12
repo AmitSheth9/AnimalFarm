0d34c0b95e12836c248f5cc8326fe6f8df711f34
Asvc9 <amit.sheth9@gmail.com>
2021-08-11 18:00:36 -0700

    started animalfarm

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-// import functions and grab DOM elements
 
-// initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log("validate")
+
+const Horsepic = document.getElementById('horse');
+const Neigh = document.getElementById('neigh');
+
+console.log(Horsepic);
+console.log(Neigh);
+
+Horsepic.addEventListener('click', () => {
+  Neigh.play() 
+});
+
+
+const Catpic = document.getElementById('cat');
+const Meow = document.getElementById('meow');
+
+Catpic.addEventListener('click', () => {
+  Meow.play();
+});
+
+const Dogpic = document.getElementById('dog');
+const Bark = document.getElementById('bark');
+
+Dogpic.addEventListener('click', () => {
+  Bark.play();
+});
