97b3c13648ee2b221561246fc9f1ecbae5c469f9
Asvc9 <amit.sheth9@gmail.com>
2021-08-12 09:21:39 -0700

    added header sound description

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ console.log("validate")
 
 const Horsepic = document.getElementById('horse');
 const Neigh = document.getElementById('neigh');
+const Animalclick = document.getElementById('animalclick')
 
 console.log(Horsepic);
 console.log(Neigh);
 
 Horsepic.addEventListener('click', () => {
   Neigh.play() 
+  Animalclick.textContent = "The sound a horse makes..."
+  console.log(Animalclick.textContent);
 });
+
 
 
 const Catpic = document.getElementById('cat');
@@ -18,6 +22,7 @@ const Meow = document.getElementById('meow');
 
 Catpic.addEventListener('click', () => {
   Meow.play();
+  Animalclick.textContent = "The sound a cat makes..."
 });
 
 const Dogpic = document.getElementById('dog');
@@ -25,4 +30,7 @@ const Bark = document.getElementById('bark');
 
 Dogpic.addEventListener('click', () => {
   Bark.play();
+  Animalclick.textContent = "The sound a dog makes..."
+  
 });
+
